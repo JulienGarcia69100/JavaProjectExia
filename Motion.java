7777b8f32c4cebd1383455b5616770585771aac4
JulienGarcia69100 <31124149+JulienGarcia69100@users.noreply.github.com>
2017-08-18 10:20:51 +0200

    Add files via upload

--- Motion.java
+++ Motion.java
@@ -2,7 +2,9 @@
 public class Motion {
 	
 	private String nameAnimal;
-	private String nameEnemy;
+	private String nameEnemy1;
+	private String nameEnemy2;
+	private String nameEnemy3;
 	private String nameFood;
 	private int satiety;
 	private int lifeExpectancy;
@@ -14,7 +16,7 @@ public class Motion {
 		
 		System.out.println("Creation of a motion animal");
 		nameAnimal = "unknown";
-		nameEnemy = "unknown";
+		nameEnemy1 = "unknown";
 		nameFood = "unknown";
 		satiety = 0;
 		lifeExpectancy = 0;
@@ -22,11 +24,13 @@ public class Motion {
 		movement = 0;
 		}
 	
-	public Motion (String pName, String pEnemy, int pMovement, int pReproduction) {
+	public Motion (String pName, String pEnemy1, String pEnemy2, String pEnemy3, int pMovement, int pReproduction) {
 		
 		System.out.println("Creation of a " + pName);
 		nameAnimal = pName;
-		nameEnemy = pEnemy;
+		nameEnemy1 = pEnemy1;
+		nameEnemy2 = pEnemy2;
+		nameEnemy3 = pEnemy3;
 		movement = pMovement;
 		reproduction = pReproduction;
 	    }
@@ -35,7 +39,7 @@ public class Motion {
 		
 		System.out.println("Creation of a " + fName);
 		nameAnimal = fName;
-		nameEnemy = fEnemy;
+		nameEnemy1 = fEnemy;
 		movement = fMovement;
 		reproduction = fReproduction;
 		nameFood = fFood;
